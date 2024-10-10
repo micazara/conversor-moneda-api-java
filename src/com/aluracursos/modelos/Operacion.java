4c1e223f1798641b8ea7db800e142b87cdac987e
Mica Zara <zara_micaela@outlook.com>
2024-10-10 15:47:26 -0300

    id autoincrementable en Operacion

--- src/com/aluracursos/modelos/Operacion.java
+++ src/com/aluracursos/modelos/Operacion.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 public class Operacion {
 
+	private static int idContador = 0;
+	private int id;
 	private double valorConvertido;
 	private double resultado;
 	private String codMonedaBase;
@@ -12,6 +14,7 @@ public class Operacion {
 
 	public Operacion(double valorConvertido, double resultado, String codMonedaBase, String codMonedaDestino,
 			String fechaHoraActual) {
+		this.id = ++idContador;
 		this.valorConvertido = valorConvertido;
 		this.resultado = resultado;
 		this.codMonedaBase = codMonedaBase;
@@ -37,8 +40,9 @@ public class Operacion {
 
 	@Override
 	public String toString() {
-		return "Operacion: valor convertido:" + valorConvertido + ", resultado:" + resultado + ", moneda base:"
-				+ codMonedaBase + ", moneda destino:" + codMonedaDestino + ", fecha y hora:" + fechaHoraActual + "\n";
+		return "Operacion: " + "Id: " + id + ", valor convertido:" + valorConvertido + ", resultado:" + resultado
+				+ ", moneda base:" + codMonedaBase + ", moneda destino:" + codMonedaDestino + ", fecha y hora:"
+				+ fechaHoraActual + "\n";
 	}
 
 }
