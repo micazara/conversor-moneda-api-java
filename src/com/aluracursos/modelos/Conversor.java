dd729e6a866abac86f1fa45736e1e5c4870b82e3
Mica Zara <zara_micaela@outlook.com>
2024-10-09 22:03:45 -0300

    menu de opciones, manejo de la solicitud y conversor funcionales

---
+++ src/com/aluracursos/modelos/Conversor.java
@@ -0,0 +1,9 @@
+package com.aluracursos.modelos;
+
+public class Conversor {
+
+	public double convertir(double conversion_rate, double valorAConvertir) {
+		return conversion_rate * valorAConvertir;
+	}
+
+}
